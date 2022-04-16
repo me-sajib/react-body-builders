922a3f514f6881cf357ed102b77cbf23b172b2c6
sajib <me.mrsajib@gmail.com>
2022-04-16 22:30:06 +0600

    offer components add

---
+++ src/Pages/Home/Offer/Offer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Offer.css";
+
+const Offer = () => {
+  return (
+    <div className="offer-area offer-bg">
+      <div className="container">
+        <div className="row justify-content-end">
+          <div className="col-lg-6">
+            <div className="offer_text">
+              <h4>A BIG OFFER FOR THIS SUMMER</h4>
+              <h3>50% Off</h3>
+              <p>
+                There are many variations of passages of lorem Ipsum available,
+                but the majority have suffered alteration.
+              </p>
+              <a href="/" className="boxed-btn3">
+                Join Now
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offer;
