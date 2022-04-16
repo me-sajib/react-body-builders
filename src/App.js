8fc577bec595d7ba418943290d7ec3385ffe8a74
sajib <me.mrsajib@gmail.com>
2022-04-16 22:35:51 +0600

    404 routes set and create components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/Home/NotFound/NotFound";
 import Nav from "./Pages/Shared/Nav/Nav";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
