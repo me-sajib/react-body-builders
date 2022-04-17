e4471635b3d85a9b3bfa6dbc7034a368949263bf
sajib <me.mrsajib@gmail.com>
2022-04-17 11:25:56 +0600

    register user successfully and show error and successful message

--- src/Pages/Login/Registration.js
+++ src/Pages/Login/Registration.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, Navigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Registration = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+  const registerUser = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.pwd.value;
+    const confirmPassword = e.target.pwd2.value;
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className="bg-dark text-light py-3">
       <div className="container my-5 w-50 mx-auto">
         <h1 className="text-center text-primary">Create your account</h1>
-        <form>
+        <form onSubmit={registerUser}>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
-            <input type="email" className="form-control" />
+            <input type="email" name="email" className="form-control" />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
             </div>
@@ -20,24 +36,56 @@ const Registration = () => {
             <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
-            <input type="password" className="form-control" />
+            <input type="password" name="pwd" className="form-control" />
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">
               Confirm Password
             </label>
-            <input type="password" className="form-control" />
+            <input type="password" name="pwd2" className="form-control" />
           </div>
+
+          {
+            // if user is register
+            user && (
+              <div className="alert alert-success">
+                <b>Registration successful.</b>{" "}
+                <Link to="/login" className="btn btn-link text-primary">
+                  Login Now
+                </Link>
+              </div>
+            )
+          }
+          {
+            // if any error occurs
+            error && <div className="alert alert-danger">{error.message}</div>
+          }
           <div className="mb-3 form-check">
             <input
               type="checkbox"
               className="form-check-input"
               id="exampleCheck1"
             />
-            <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
+            <label
+              className="form-check-label text-danger"
+              htmlFor="exampleCheck1"
+            >
+              Agree term and conditions
             </label>
           </div>
+
+          {
+            // if loading is true, show a spinner
+            loading ? (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            ) : (
+              ""
+            )
+          }
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
