4c21b773ae4682276bb24869dd48f04395345fd9
sajib <me.mrsajib@gmail.com>
2022-04-17 17:15:39 +0600

    services section loading spinner add

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../../Shared/Loading/Loading";
 import Service from "./Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/me-sajib/json/main/services.json")
       .then((res) => res.json())
       .then((data) => setServices(data));
+    setLoading(false);
   }, []);
   return (
     <div className="container py-4">
       <h1 className="text-info text-center py-3">Our Services</h1>
+      {
+        // if loading is true then show the loader
+        loading && <Loading />
+      }
       <div className="row row-cols-1 row-cols-md-2 g-4 ">
         {services.map((services) => (
           <Service key={services.id} services={services} />
