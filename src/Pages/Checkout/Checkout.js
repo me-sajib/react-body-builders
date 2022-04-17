792b4c75bcdacb8738e3e16e15c5f988f9a84b07
sajib <me.mrsajib@gmail.com>
2022-04-17 17:07:29 +0600

    checkout design done and show all details

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
 const Checkout = () => {
+  const [services, setService] = useState([]);
+  const [enroll, setEnroll] = useState(false);
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/me-sajib/json/main/services.json")
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
+  const id = useParams().id;
+  const service = services.find((s) => s.id === id);
+
+  const enrollCourse = () => {
+    toast("Course Enrolled Successfully");
+    setEnroll(true);
+  };
   return (
-    <div className="container">
-      <h1 className=" text-center">Best Of Your Course</h1>
-      <p></p>
+    <div className="bg-dark">
+      <div className="container py-5">
+        <h1 className="text-center text-light py-4">
+          Enroll <span className="text-primary"> {service?.name}</span> Course
+        </h1>
+        <div className="row row-cols-1 row-cols-md-2 g-4 ">
+          <div className="col">
+            <img className="w-100" src={service?.image} alt="" />
+          </div>
+          <div className="col">
+            <h1 className="text-primary">{service?.name}</h1>
+            <h3 className="text-info">Price: ${service?.price}</h3>
+            <p className="text-muted">{service?.description}</p>
+            <p className="text-primary">{service?.details}</p>
+
+            {
+              // if enroll is true then show the button
+              enroll ? (
+                <p className="text-info">already enrolled</p>
+              ) : (
+                <button
+                  onClick={enrollCourse}
+                  className="btn btn-primary d-block w-75 mx-auto"
+                >
+                  ENROLL
+                </button>
+              )
+            }
+          </div>
+          <ToastContainer />
+        </div>
+      </div>
     </div>
   );
 };
