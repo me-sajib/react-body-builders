b1d7533e7af466386dfa0273a426c30c95b9e5a8
sajib <me.mrsajib@gmail.com>
2022-04-17 13:12:44 +0600

    folder stucktureing and text edit

---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container py-5">
+      <h1 className="text-center my-5 text-danger">
+        404 !! <br /> URL not found
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
