be3eef314f3e09d01db49a95d5b2c88da8e1e5d1
sajib <me.mrsajib@gmail.com>
2022-04-17 13:16:35 +0600

    checkout page added

---
+++ src/Pages/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div className="container">
+      <h1 className=" text-center">Best Of Your Course</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default Checkout;
