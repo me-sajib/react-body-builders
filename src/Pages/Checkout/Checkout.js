baca8815f62d3da61b86db745948770ebebc0b87
sajib <me.mrsajib@gmail.com>
2022-04-17 17:20:27 +0600

    checkout pages loading spinner add

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
+import Loading from "../Shared/Loading/Loading";
 
 const Checkout = () => {
   const [services, setService] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [enroll, setEnroll] = useState(false);
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/me-sajib/json/main/services.json")
       .then((res) => res.json())
       .then((data) => setService(data));
+    setLoading(false);
   }, []);
   const id = useParams().id;
   const service = services.find((s) => s.id === id);
@@ -20,6 +23,10 @@ const Checkout = () => {
   return (
     <div className="bg-dark">
       <div className="container py-5">
+        {
+          // if loading is true then show the loader
+          loading && <Loading />
+        }
         <h1 className="text-center text-light py-4">
           Enroll <span className="text-primary"> {service?.name}</span> Course
         </h1>
