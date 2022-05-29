b6d683d87f3d922f945e8dffd58a63041925dd4b
sajib <me.mrsajib@gmail.com>
2022-05-29 15:25:16 +0600

    spinner change

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -2,13 +2,9 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <>
-      <div className="d-flex justify-content-center">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    </>
+    <div className="spinner-grow text-info" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
   );
 };
 
